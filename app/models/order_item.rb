3223ccacfbc5b49e8f1964837f0b1f9f69fa3110
Winston Chu <wpchu@andrew.cmu.edu>
2017-03-22 20:17:24 -0400

    started order items model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,29 @@
 class OrderItem < ActiveRecord::Base
+
+    # Relationships
+    # -----------------------------
+    belongs_to :item
+    belongs_to :order
+
+    # Scopes
+    # -----------------------------
+
+
+    # Validations
+    # -----------------------------
+    # restricts item_ids to items which exist and are active in the system
+    validate :item_is_active_in_system
+
+    # Methods
+    # -----------------------------
+
+    private
+
+    def item_is_active_in_system
+      all_active_items = Item.active.all.map(&:id)
+      unless all_active_items.include?(self.item_id)
+        errors.add(:item_id, "is not an active item at the chess store")
+      end
+    end
+
 end
