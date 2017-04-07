5fcd3545e2073b78bf0292d197d06c55edcf5bfc
Winston Chu <wpchu@andrew.cmu.edu>
2017-04-07 16:05:47 -0400

    changed order of calling stuff

--- test/contexts.rb
+++ test/contexts.rb
@@ -1,21 +1,21 @@
 # require needed files
 require './test/sets/items'
 require './test/sets/item_prices'
+require './test/sets/orders'
+require './test/sets/order_items'
 require './test/sets/purchases'
 require './test/sets/schools'
 require './test/sets/users'
-require './test/sets/orders'
-require './test/sets/order_items'
 require './test/sets/credit_cards'
 
 module Contexts
   # explicitly include all sets of contexts used for testing 
   include Contexts::Items
   include Contexts::ItemPrices
+  include Contexts::Orders
+  include Contexts::OrderItems
   include Contexts::Purchases
   include Contexts::Schools
   include Contexts::Users
-  include Contexts::Orders
-  include Contexts::OrderItems
   include Contexts::CreditCards
 end
